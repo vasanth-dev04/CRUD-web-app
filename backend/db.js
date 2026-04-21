e6a6d6f354d54e0beb5abae40e4ec78756256ad8
Marivasanth <mmarivasanth3@gmail.com>
2026-04-21 23:23:19 +0530

    fix db connection timeout

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,25 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-// Connection string
 const db = mysql.createPool({
-    host:process.env.DB_HOST || "localhost",
-    user:process.env.DB_USER || "root",
-    password:process.env.DB_PASSWORD || "",
-    database:process.env.DB_NAME || "sample_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+        rejectUnauthorized: true
+    },
+    connectTimeout: 30000,
+
 });
 
-// Test connection
-
-(async()=>{
+(async () => {
     try {
         const connection = await db.getConnection();
-        console.log("Server is running...");
+        console.log("Database connected successfully!");
         connection.release();
-    } catch(error){
+    } catch(error) {
         console.error("MySQL connection failed:", error);
     }
 })();
